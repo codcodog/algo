7f38cc4cfcea2b06308a48691563974e6e3407ce
codcodog <codcodog@gmail.com>
2019-05-29 19:42:49 +0800

    queue

---
+++ data-structure/queue/queue_test.go
@@ -0,0 +1,29 @@
+package queue
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEnqueue(t *testing.T) {
+	q := New(3)
+	q.Enqueue(1)
+	q.Enqueue(2)
+	q.Enqueue(3)
+	q.Print()
+
+	q.Enqueue(4)
+	q.Print()
+}
+
+func TestDequeue(t *testing.T) {
+	q := New(3)
+	q.Enqueue(1)
+	q.Enqueue(2)
+	q.Enqueue(3)
+
+	first := q.Dequeue()
+	assert.Equal(t, 1, first)
+	q.Print()
+}
