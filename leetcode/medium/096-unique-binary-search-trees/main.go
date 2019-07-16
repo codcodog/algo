d31245bc0cbddfb9e94f9be6d71b887717258b28
codcodog <codcodog@gmail.com>
2019-07-16 14:10:32 +0800

    leetcode: unique binary search trees

---
+++ leetcode/medium/096-unique-binary-search-trees/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	input := 3
+	fmt.Println(numTrees(input)) // expected: 5
+}
+
+// F(i,n)= G(i−1) * G(n−i)
+func numTrees(n int) int {
+	G := make(map[int]int)
+	G[0], G[1] = 1, 1
+
+	for i := 2; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			G[i] += G[j-1] * G[i-j]
+		}
+	}
+	return G[n]
+}
